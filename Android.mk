dd255aa642055601079dd2d7829c66a6b7510897
Jackeagle <jackeagle102@gmail.com>
2022-03-19 04:22:08 -0400

    I001D: Create mountpoint/symlink for asusfw
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -8,4 +8,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),I01WD)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(CLEAR_VARS)
+
+ASUSFW_MOUNT_POINT_SYMLINK := $(TARGET_OUT_VENDOR)/asusfw
+$(ASUSFW_MOUNT_POINT_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $@ link"
+	@rm -rf $@
+	$(hide) ln -sf /mnt/vendor/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(ASUSFW_MOUNT_POINT_SYMLINK)
+
 endif
